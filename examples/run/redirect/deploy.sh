03e51ec5b10eedc473c8ce070993a3c2f5a5627a
nathants <me@nathants.com>
2020-01-09 16:18:24 -0800

    run api is not beta

--- examples/run/redirect/deploy.sh
+++ examples/run/redirect/deploy.sh
@@ -5,7 +5,7 @@ project=$(gcloud config get-value project)
 container=gcr.io/$project/run-redirect:latest
 docker build . -t $container
 docker push $container
-gcloud beta run deploy \
+gcloud run deploy \
        run-redirect \
        --allow-unauthenticated \
        --region us-central1 \
